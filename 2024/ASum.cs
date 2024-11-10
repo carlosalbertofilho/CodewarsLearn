54efff547c8d761727007dfc771e708351d9970c
carlosalberto_filho <carlosalberto_filho@outlook.com>
2024-11-10 09:58:52 -0300

    fix

--- 2024/ASum.cs
+++ 2024/ASum.cs
@@ -7,7 +7,7 @@ namespace _2024;
 
 You are given the total volume m of the building. Being given m can you find the number n of cubes you will have to build?
 
-The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m and you have to return the integer n such as 
+The parameter of the function findNb (find_nb, find-nb, findNb, ...) will be an integer m, and you have to return the integer n such as 
 n^3 + (n-1)^3 + (n-2)^3 + ... + 1 if such an n exists or -1 if there is no such n.
 
 Examples:
